2477a13bb5fe47666fc1e3235113af10871fd24d
dhanushakasani <97589416+dhanushakasani@users.noreply.github.com>
2022-02-28 12:54:28 +0530

    Add files via upload

--- EProduct.java
+++ EProduct.java
@@ -1,21 +1,20 @@
+package com.ecommerce.entity;
+
 import java.math.BigDecimal;
 import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;  
+
 
 public class EProduct {
+
         private long ID;
         private String name;
         private BigDecimal price;
-        private Date dateAdded;
-        
-        public EProduct() {
-                
-        }
-        public EProduct(long id, String name, BigDecimal price, Date dateAdded) {
-                this.ID = id;
-                this.name = name;
-                this.price = price;
-                this.dateAdded = dateAdded;
-        }
+        private Date dateAdded;  
         
         public long getID() {return this.ID; }
         public String getName() { return this.name;}
@@ -25,8 +24,5 @@ public class EProduct {
         public void setID(long id) { this.ID = id;}
         public void setName(String name) { this.name = name;}
         public void setPrice(BigDecimal price) { this.price = price;}
-        public void setDateAdded(Date date) { this.dateAdded = date;}
-        
-        
+        public void setDateAdded(Date date) { this.dateAdded = date;}   
 }
-
--- LoginServlet.java
+++ LoginServlet.java
@@ -1,31 +1,42 @@
-package assistedPrgram6.org.company;
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.*;
+import javax.servlet.http.*;
 
-import java.io.IOException;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
+/**
+* Servlet implementation class LoginServlet
+*/
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-        
+        private static final long serialVersionUID = 1L;
+       
+    /**
+* @see HttpServlet#HttpServlet()
+*/
+    public LoginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
                 // TODO Auto-generated method stub
                 
-                 Cookie userId = new Cookie("userid", request.getParameter("userid"));
-                 response.addCookie(userId);
-              response.sendRedirect("DashBoard");  
+                 String userId = request.getParameter("userid");
+             response.sendRedirect("dashboard?userid=" + userId);  
                 
         }
 
-       
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
         protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
                 // TODO Auto-generated method stub
                 doGet(request, response);
         }
 
 }
-
